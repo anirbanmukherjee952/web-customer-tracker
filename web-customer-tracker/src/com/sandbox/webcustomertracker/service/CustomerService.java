257fc2024db943d56f460a360646c9611d2e9eb8
Anirban <Anirban@Pheonix>
2021-04-29 20:03:38 +0530

    Version 2.0:
    added Service Layer, between controller and DAO layers

---
+++ web-customer-tracker/src/com/sandbox/webcustomertracker/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.sandbox.webcustomertracker.service;
+
+import java.util.List;
+
+import com.sandbox.webcustomertracker.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+	
+}
---
+++ web-customer-tracker/src/com/sandbox/webcustomertracker/service/CustomerServiceImpl.java
@@ -0,0 +1,24 @@
+package com.sandbox.webcustomertracker.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sandbox.webcustomertracker.dao.CustomerDAO;
+import com.sandbox.webcustomertracker.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		return customerDAO.getCustomers();
+	}
+
+}
